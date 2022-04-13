8c9b9243fc87e41b1cf83ddaf6f1979b69118dd0
will <wbrycejohnson031086@gmail.com>
2022-04-13 13:49:58 -0400

    add favicon; add icon; change directory structure

--- myapp/core/views.py
+++ myapp/core/views.py
@@ -3,12 +3,12 @@ from myapp.models import Book
 
 core = Blueprint('core', __name__)
 
-@core.route('/')
+@core.route('/info')
 def index():
   page = request.args.get('page', 1, type=int)
   books = Book.query.order_by(Book.date.desc()).paginate(page=page, per_page=5)
-  return render_template('index.html', books=books)
+  return render_template('info.html', books=books)
 
-@core.route('/info')
+@core.route('/')
 def info():
-  return render_template('info.html')
+  return render_template('index.html')
